8948d40faebf02c9bfddb5cd0ff4b4fc5cfe8c65
Imene <imene.bousmaha@outlook.fr>
2023-03-07 09:57:37 +0100

    Ajout des fichiers de travail

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ("express") ; // initialisation module
+const app = express() ;
+const port = 3000 ;
+
+function main(){
+
+    app.get("/", (req, res) => {
+        const currentDate = new Date();
+        console.log("page called at" + currentDate);
+        res.send("Hello World ! Nous sommes le " + currentDate);
+    });
+
+    app.listen(port, function() {
+        console.log(`Example app listening on port ${port}`);
+    });
+}
+
+main();
